fef2b879c058888079868e81df9bddac8da05cbd
Kreshnik Nikci <34795384+Kr3wg@users.noreply.github.com>
2022-07-01 16:22:09 -0400

    Update looping.js

--- tests/02_looping/looping.js
+++ tests/02_looping/looping.js
@@ -1,11 +1,20 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function factorial(num) {
-
+let total = 1;
+  for (let i = 1 ; i <= num ; i++) {
+    total *= i ;
+  }
+  return total ;
 }
 
 function stringReverse(str) {
+let rev = '';
 
+  for (let i = str.length -1 ; i >= 0 ; i--){
+    rev += str[i];
+  }
+  return rev
 }
 
 // do not use built in slice method.
